6134ced46e93dc8a056c9e8036bf439e84fd4424
shail <shail@LAPTOP-J2GBEUT9>
2022-06-11 16:28:33 +0530

    "first change"

--- june22/src/june22/M1.java
+++ june22/src/june22/M1.java
@@ -4,7 +4,7 @@ public class M1 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("shailesh");
+		System.out.println("shailesh kalyane");
 
 	}
 
--- june22/src/june22/M2.java
+++ june22/src/june22/M2.java
@@ -5,7 +5,7 @@ public class M2
 
 	public static void main(String[] args) 
 	{
-		System.out.println("sourab");
+		System.out.println("sourab ");
 
 	}
 
