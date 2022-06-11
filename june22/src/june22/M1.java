bfff199fab732ab276229fb39402d7505856ee8d
shail <shail@LAPTOP-J2GBEUT9>
2022-06-11 16:17:30 +0530

    "hi this my first commit"

---
+++ june22/src/june22/M1.java
@@ -0,0 +1,11 @@
+package june22;
+
+public class M1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("shailesh");
+
+	}
+
+}
---
+++ june22/src/june22/M2.java
@@ -0,0 +1,12 @@
+package june22;
+
+public class M2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("sourab");
+
+	}
+
+}
